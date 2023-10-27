368eccf0821de2038e9119f31392d28efe529a2a
Michele Tobias <mmtobias@ucdavis.edu>
2023-10-27 14:31:00 -0700

    rstac landsat search

--- r/exploration.R
+++ r/exploration.R
@@ -1,7 +1,8 @@
 # GOAL: explore options for downloading and processing landsat or HLS data for California coastal dune systems
 
 # Citations:
-# https://brazil-data-cube.github.io/rstac/
+# rstac Example: https://brazil-data-cube.github.io/rstac/
+# rstac Documentation: https://brazil-data-cube.github.io/rstac/reference/index.html 
 
 
 
@@ -14,6 +15,11 @@
 library(rstac) # rstac is a client for finding and downloading data stored in a SpatioTemporal Asset Catalog (rstac) and available trough an API
 
 
+
+
+# SEARCH LANDSAT ----------------------------------------------------------
+
+
 # connect to the lansat stac catalog endpoint
 landsat_stac <- stac("https://landsatlook.usgs.gov/stac-server")
 
@@ -22,18 +28,27 @@ get_request(landsat_stac)
 landsat_collections <- get_request(collections(landsat_stac))
 
 
-
+#set up the search parameters
 search_landsat <- stac_search(
   q = landsat_stac,
   collections = "landsat-c2l2-sr",
   ids = NULL,
-  bbox = c( -123.824405, 39.485343, -123.748531, 39.556319),  # likely: minimum longitude, minimum latitude, maximum longitude, and maximum latitude --- NW: 39.562010, -123.823032  SE: 39.486138, -123.734455
+  bbox = c( -123.824405, 39.485343, -123.748531, 39.556319),  # minimum longitude, minimum latitude, maximum longitude, and maximum latitude --- 
   datetime = "2023-06-01T00:00:00Z/2023-06-30T00:00:00Z",  # A closed interval: "2018-02-12T00:00:00Z/2018-03-18T12:31:12Z" 
   intersects = NULL,
   limit = 100
 )
 
-get_request(search_landsat)
+# run the search
+results_landsat <- get_request(search_landsat)
+
+# see what the results are from our search
+results_landsat
+
+
+
+
+# SEARCH HLS ----------------------------------------------------------
 
 # connect to the HLS stac catalog endpoint
 #hls_stac <- stac("") 
