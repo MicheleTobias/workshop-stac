b5b59f9a9b5ce379f97fb6142ce1774053fec661
Michele Tobias <mmtobias@ucdavis.edu>
2023-10-31 09:59:28 -0700

    add example

--- r/configure_rgdal.R
+++ r/configure_rgdal.R
@@ -8,3 +8,10 @@ setGDALconfig("GDAL_HTTP_UNSAFESSL", "YES")
 setGDALconfig("GDAL_HTTP_COOKIEFILE", ".rcookies") 
 setGDALconfig("GDAL_DISABLE_READDIR_ON_OPEN", "EMPTY_DIR")
 setGDALconfig("CPL_VSIL_CURL_ALLOWED_EXTENSIONS", "TIF") 
+
+
+# example of downloading from a STAC URL:
+
+# url <- "/vsicurl/https://data.lpdaac.earthdatacloud.nasa.gov/lp-prod-protected/HLSS30.020/HLS.S30.T10SEJ.2021214T184919.v2.0/HLS.S30.T10SEJ.2021214T184919.v2.0.B8A.tif" 
+
+# r <- rast(url)
